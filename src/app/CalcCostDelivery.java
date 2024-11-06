0ba8ca615d3d605f0364beebf42fc586da432db5
Nikolaev Igor <nikolaevin91@gmail.com>
2024-11-06 08:19:53 +0200

    Modify files

---
+++ src/app/CalcCostDelivery.java
@@ -0,0 +1,12 @@
+package app;
+
+public class CalcCostDelivery extends CalcCostBase{
+
+    private final static double deliveryPrice = 7;
+
+    @Override
+    public double calcCost(Product product) {
+        return product.getQuota() * product.getPrice()
+                + deliveryPrice;
+    }
+}
